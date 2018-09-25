e08b1a170ca78b652554497a65871ed1f8633714
bodinho-of-war <luanpb@live.com>
2018-09-25 00:16:29 -0300

    Create Table

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const knex = require('knex')({
-  client: 'mysql',
-  connection: {
+let conn{
     host : '127.0.0.1',
     user : 'sa',
     password : '123@abcd',
-    database : 'UpdateTest'
-  }
-});
+    charset  : 'utf8'
+};
+
+let knex = require('knex')( { client: 'mysql', connection: conn});
+
+knex.raw('CREATE DATABASE teste_update')
+  .then(() => {
+    knex.destroy();
+
+    knex = require('knex')( { client: 'mysql', connection: conn});
+
+    knex.schema.createTable('pessoa', table => {
+      table.string('nome','sobreNome');
+    }).then(() => {
+        knex.destroy();
+    });
+  });
